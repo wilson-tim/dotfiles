73ba88d5efdef61d6874d92d2211c5a1a220add6
Tim Wilson <tgwilson243@gmail.com>
2021-07-08 14:15:33 +0100

    Improve create_links.cmd with variable %DOTFILES%

--- create_links.cmd
+++ create_links.cmd
@@ -6,61 +6,63 @@ REM 13/11/2020  TW  Add link for .gitconfig_local
 
 @ECHO OFF
 
+SET DOTFILES=C:\Users\Tim Wilson\OneDrive - Business Thinking Limited\Documents\projects\dotfiles
+
 REM Git Bash vim
 REM
 if exist "%USERPROFILE%\.vim\" (rmdir "%USERPROFILE%\.vim")
-mklink /d "%USERPROFILE%\.vim" "%USERPROFILE%\Documents\projects\dotfiles\.vim"
+mklink /d "%USERPROFILE%\.vim" "%DOTFILES%\.vim"
 
 REM gVim
 REM
 if exist "%USERPROFILE%\vimfiles\" (rmdir "%USERPROFILE%\vimfiles")
-mklink /d "%USERPROFILE%\vimfiles" "%USERPROFILE%\Documents\projects\dotfiles\.vim"
+mklink /d "%USERPROFILE%\vimfiles" "%DOTFILES%\.vim"
 
 REM %USERPROFILE%
 REM
 if exist "%USERPROFILE%\.bash_profile" (echo Y|del "%USERPROFILE%\.bash_profile")
-mklink "%USERPROFILE%\.bash_profile" "%USERPROFILE%\Documents\projects\dotfiles\.bash_profile"
+mklink "%USERPROFILE%\.bash_profile" "%DOTFILES%\.bash_profile"
 
 if exist "%USERPROFILE%\.bashrc" (echo Y|del "%USERPROFILE%\.bashrc")
-mklink "%USERPROFILE%\.bashrc" "%USERPROFILE%\Documents\projects\dotfiles\.bashrc"
+mklink "%USERPROFILE%\.bashrc" "%DOTFILES%\.bashrc"
 
 if exist "%USERPROFILE%\.gitconfig" (echo Y|del "%USERPROFILE%\.gitconfig")
-mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\Documents\projects\dotfiles\.gitconfig"
+mklink "%USERPROFILE%\.gitconfig" "%DOTFILES%\.gitconfig"
 
 if exist "%USERPROFILE%\.gitconfig_local" (echo Y|del "%USERPROFILE%\.gitconfig_local")
-mklink "%USERPROFILE%\.gitconfig_local" "%USERPROFILE%\Documents\projects\dotfiles\.gitconfig_local"
+mklink "%USERPROFILE%\.gitconfig_local" "%DOTFILES%\.gitconfig_local"
 
 if exist "%USERPROFILE%\.minttyrc" (echo Y|del "%USERPROFILE%\.minttyrc")
-mklink "%USERPROFILE%\.minttyrc" "%USERPROFILE%\Documents\projects\dotfiles\.minttyrc"
+mklink "%USERPROFILE%\.minttyrc" "%DOTFILES%\.minttyrc"
 
 if exist "%USERPROFILE%\.tmux_conf" (echo Y|del "%USERPROFILE%\.tmux_conf")
-mklink "%USERPROFILE%\.tmux_conf" "%USERPROFILE%\Documents\projects\dotfiles\.tmux_conf"
+mklink "%USERPROFILE%\.tmux_conf" "%DOTFILES%\.tmux_conf"
 
 if exist "%USERPROFILE%\.vimrc" (echo Y|del "%USERPROFILE%\.vimrc")
-mklink "%USERPROFILE%\.vimrc" "%USERPROFILE%\Documents\projects\dotfiles\.vimrc"
+mklink "%USERPROFILE%\.vimrc" "%DOTFILES%\.vimrc"
 
 REM git
 REM
 if exist "C:\Program Files\Git\etc\profile" (echo Y|del "C:\Program Files\Git\etc\profile")
-mklink "C:\Program Files\Git\etc\profile" "%USERPROFILE%\Documents\projects\dotfiles\git\profile"
+mklink "C:\Program Files\Git\etc\profile" "%DOTFILES%\git\profile"
 
 REM tmux
 REM
 if exist "C:\Program Files\Git\usr\bin\tmux.exe" (echo Y|del "C:\Program Files\Git\usr\bin\tmux.exe")
-mklink "C:\Program Files\Git\usr\bin\tmux.exe" "%USERPROFILE%\Documents\projects\dotfiles\tmux\tmux.exe.3.0a"
+mklink "C:\Program Files\Git\usr\bin\tmux.exe" "%DOTFILES%\tmux\tmux.exe.3.0a"
 
 if exist "C:\Program Files\Git\usr\bin\msys-event-2-1-6.dll" (echo Y|del "C:\Program Files\Git\usr\bin\msys-event-2-1-6.dll")
-mklink "C:\Program Files\Git\usr\bin\msys-event-2-1-6.dll" "%USERPROFILE%\Documents\projects\dotfiles\tmux\msys-event-2-1-6.dll"
+mklink "C:\Program Files\Git\usr\bin\msys-event-2-1-6.dll" "%DOTFILES%\tmux\msys-event-2-1-6.dll"
 
 if exist "C:\Program Files\Git\usr\bin\msys-event-2-1-7.dll" (echo Y|del "C:\Program Files\Git\usr\bin\msys-event-2-1-7.dll")
-mklink "C:\Program Files\Git\usr\bin\msys-event-2-1-7.dll" "%USERPROFILE%\Documents\projects\dotfiles\tmux\msys-event-2-1-7.dll"
+mklink "C:\Program Files\Git\usr\bin\msys-event-2-1-7.dll" "%DOTFILES%\tmux\msys-event-2-1-7.dll"
 
 REM nvim
 REM
 if exist "%USERPROFILE%\AppData\Local\nvim\ginit.vim" (echo Y|del "%USERPROFILE%\AppData\Local\nvim\ginit.vim")
-mklink %USERPROFILE%\AppData\Local\nvim\ginit.vim "%USERPROFILE%\Documents\projects\dotfiles\nvim\ginit.vim"
+if exist "%USERPROFILE%\AppData\Local\nvim" (mklink "%USERPROFILE%\AppData\Local\nvim\ginit.vim" "%DOTFILES%\nvim\ginit.vim")
 
 if exist "%USERPROFILE%\AppData\Local\nvim\init.vim" (echo Y|del "%USERPROFILE%\AppData\Local\nvim\init.vim")
-mklink %USERPROFILE%\AppData\Local\nvim\init.vim "%USERPROFILE%\Documents\projects\dotfiles\nvim\init.vim"
+if exist "%USERPROFILE%\AppData\Local\nvim" (mklink "%USERPROFILE%\AppData\Local\nvim\init.vim" "%DOTFILES%\nvim\init.vim")
 
 REM
